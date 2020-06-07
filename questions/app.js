e381aa1db5e2a2f3f41019b531a84fe0881c7424
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-07 14:18:01 +0100

    Add menu filter functionality

--- questions/app.js
+++ questions/app.js
@@ -1,9 +1,33 @@
 // TRAVERSING THE DOM
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text')
+// btns.forEach(btn => {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text')
+//     })
+// })
+
+
+
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn')
+    // console.log(btn);
+
+
+
+    btn.addEventListener('click', () => {
+        // as you are clicking the button, you loop through the item so you close all other questions apart from the one you are clicking
+            questions.forEach(item => {
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text');
     })
-})
+
+});
